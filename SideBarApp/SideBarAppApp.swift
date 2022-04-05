7fdf039a1e54143d88e1e76fb4b5b99be191340a
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-05 15:41:17 +0530

    Initial Commit

---
+++ SideBarApp/SideBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBarAppApp.swift
+//  SideBarApp
+//
+//  Created by Bhumika Patel on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SideBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
